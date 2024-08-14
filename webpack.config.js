54d6be7aae26eb0a3acbbf0800485ec3b2c62f2d
EgorZaprivarin <ezaprivarin@mail.ru>
2024-08-14 18:06:45 +0300

    rename files

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: "development",
-    entry: './scripts.js',
+    entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
