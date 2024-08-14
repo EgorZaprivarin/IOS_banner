39baf0af99d2b8f5342f3d84ec40e4374a1c0652
EgorZaprivarin <ezaprivarin@mail.ru>
2024-08-14 16:29:29 +0300

    Test task

---
+++ src/languages.js
@@ -0,0 +1,15 @@
+import en from "./lang/en.json";
+import es from "./lang/es.json";
+import fr from "./lang/fr.json";
+import de from "./lang/de.json";
+import ja from "./lang/ja.json";
+import pt from "./lang/pt.json";
+
+export const languages = {
+    en,
+    es,
+    fr,
+    de,
+    ja,
+    pt,
+};
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/scripts.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "images/[hash][ext][query]",
+                },
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: './src/styles.css'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
